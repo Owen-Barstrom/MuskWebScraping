963f44b4c08b7cc6e4183f2c28e037f4334a958e
Owen-Barstrom <obarstrom@gmail.com>
2025-07-27 12:56:56 -0400

    Update getMuskInfo.py

--- getMuskInfo.py
+++ getMuskInfo.py
@@ -3,14 +3,14 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-#from selenium.webdriver.chrome.service import Service as FirefoxService
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
 def getTweets(page):
     ready = ""
     settings = Options()
     settings.headless = True
-    #settings.binary_location = '/snap/bin/firefox'
+    settings.add_argument("--disable-gpu")
     service = webdriver.ChromeService(executable_path='/usr/bin/chromedriver')
     settings.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=settings, service=service)
